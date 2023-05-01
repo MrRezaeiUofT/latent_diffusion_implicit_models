b1bddb2dc376717a4d0fd9599c793e472d63f776
mohammad.rezaei <m.reza.rezaei72@gmail.com>
2023-05-01 17:14:34 -0400

    LIDM v1

--- LIDM.py
+++ LIDM.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
 import numpy as np
-
+import torch.nn.functional as F
 import random
-
+from torch.utils.data import Dataset
 SEED = 1234
-
+from torch.autograd import Variable
 random.seed(SEED)
 np.random.seed(SEED)
 torch.manual_seed(SEED)
@@ -13,32 +13,24 @@ torch.cuda.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 class g_theta(nn.Module):
-    def __init__(self, input_dim, output_dim, n_layers, dropout, bidirectional =False ):
+    def __init__(self, input_dim,
+                 output_dim,
+                 n_layers,
+                 dropout):
         super().__init__()
 
         self.output_dim = output_dim
-        self.n_layers = n_layers
+
         self.input_dim=input_dim
         self.embedding = nn.Sequential(nn.Linear(input_dim, input_dim),nn.Linear(input_dim, output_dim))
-        self.rnn = nn.LSTM(input_dim, input_dim, n_layers, dropout=dropout, bidirectional =bidirectional )
-
+        # self.rnn = nn.LSTM(input_dim, input_dim, n_layers, dropout=dropout, bidirectional =bidirectional )
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, src):
         # src = [src len, batch size]
-        # embedded = self.mixer(embedded)
-        # embedded = [src len, batch size, out dim]
-
-        # embedded=torch.concatenate((embedded,src[:,:,:1]), axis=-1)
-        outputs, (hidden,cell) = self.rnn(src)
-
-        # outputs = [src len, batch size, input dim * n directions]
-        # hidden = [n layers * n directions, batch size, input dim]
-        # cell = [n layers * n directions, batch size, input dim]
-
-        # outputs are always from the top hidden layer
-        outputs = self.dropout(self.embedding(outputs))
-        return hidden, cell, outputs
+        outputs = self.dropout(self.embedding(src))
+        # outputs = [src len, batch size, observation dim]
+        return outputs
 
 class f_phi_x(nn.Module):
     def __init__(self, input_dim, output_dim, n_layers, dropout ):
@@ -58,13 +50,13 @@ class f_phi_x(nn.Module):
         return embedded
 
 class f_phi(nn.Module):
-    def __init__(self, obser_dim, latent_dim, n_layers, dropout, bidirectional ):
+    def __init__(self, obser_dim, latent_dim, n_layers,alpha, dropout, bidirectional ):
         super().__init__()
         self.obser_dim = obser_dim
         self.latent_dim = latent_dim
         self.n_layers = n_layers
         self.bidirectional = bidirectional
-        self.alpha = .5
+        self.alpha = alpha
         self.f_phi_x= f_phi_x(input_dim=obser_dim,
                               output_dim=latent_dim,
                               n_layers=2,
@@ -82,10 +74,12 @@ class f_phi(nn.Module):
 
     def forward(self, x_k, hidden, cell):
 
-        embedded = self.dropout(self.embedding_z(hidden))
+
 
         # embedded = [1, batch size, latent dim]
-        embedded_new= torch.sqrt(self.alpha)*self.f_phi_x(x_k) + torch.sqrt(1-self.alpha)*embedded
+        embedded_new= torch.sqrt(self.alpha)*self.f_phi_x(x_k)# + torch.sqrt(1-self.alpha)*hidden
+        embedded_new = embedded_new.unsqueeze(0)
+        # embedded_new = self.dropout(self.embedding_z(embedded_new))
         output, (hidden, cell) = self.rnn(embedded_new, (hidden,cell))
 
         # output = [seq len, batch size, out dim * n directions]
@@ -99,22 +93,27 @@ class f_phi(nn.Module):
         return prediction, hidden, cell
 
 class LIDM(nn.Module):
-    def __init__(self, latent_dim, obser_dim, device):
+    def __init__(self, latent_dim, obser_dim, sigma_x, sigma_z, device):
         super().__init__()
         self.latent_dim=latent_dim
         self.obser_dim =obser_dim
         self.device=device
+        self.sigma_x=torch.tensor([sigma_x], requires_grad=False )
+        self.sigma_z=torch.tensor([sigma_z], requires_grad=False )
+        self.alpha = torch.tensor([.5], requires_grad=False)
+        self.n_layers=2
+        self.dp_rate=.1
         self.f_phi = f_phi(obser_dim=self.obser_dim,
                           latent_dim=self.latent_dim,
-                          n_layers=2,
-                          dropout=.1,
+                          n_layers= self.n_layers,
+                           alpha=self.alpha,
+                          dropout=self.dp_rate,
                           bidirectional=False)
 
         self.g_theta = g_theta( input_dim=self.latent_dim,
                                 output_dim=self.obser_dim,
-                                n_layers=2,
-                                dropout=.1,
-                                bidirectional =False)
+                                n_layers= self.n_layers,
+                                dropout=self.dp_rate)
 
 
     def forward(self, obsrv):
@@ -122,9 +121,9 @@ class LIDM(nn.Module):
         # trg = [trg len, batch size]
         # teacher_forcing_ratio is probability to use teacher forcing
         # e.g. if teacher_forcing_ratio is 0.75 we use ground-truth inputs 75% of the time
-
-        batch_size = obsrv.shape[1]
-        seq_len = obsrv.shape[0]
+        self.obsrv = obsrv
+        batch_size = self.obsrv.shape[1]
+        seq_len = self.obsrv.shape[0]
 
         # tensor to store decoder outputs
         self.outputs = torch.zeros(seq_len, batch_size, self.latent_dim).to(self.device)
@@ -132,21 +131,38 @@ class LIDM(nn.Module):
 
         # last hidden state of the encoder is used as the initial hidden state of the decoder
 
-        z = torch.distributions.MultivariateNormal(np.zeros(self.latent_dim), np.eye(self.latent_dim)).sample((1,))
-
+        z= Variable(torch.randn((self.n_layers,batch_size, self.latent_dim))).to(self.device)
+        cell=torch.zeros_like(z)
         for k in range(1, seq_len):
 
-            output, z, cell = self.f_phi(obsrv[k], z, cell)
+            output, z, cell = self.f_phi(self.obsrv[k], z, cell)
 
             # place predictions in a tensor holding predictions for each token
             self.outputs[k] = output
 
         return self.outputs
-    def loss (self, observations):
+    def loss (self):
+        L1=F.mse_loss(self.obsrv[1:], self.g_theta(self.outputs[:-1]))
+        L2=F.mse_loss(torch.sqrt(self.alpha)*self.f_phi.f_phi_x(self.obsrv[1:]),
+                      self.outputs[1:]-torch.sqrt(1-self.alpha)*self.outputs[:-1])
+
+        L=L2+ L1*torch.pow(self.sigma_x,2)/(torch.pow(self.sigma_z,2)*torch.sqrt(self.alpha))
+        return L
 
 
 
+class get_dataset(Dataset):
 
+    ''' create a dataset suitable for pytorch models'''
+    def __init__(self, x,z, device):
+        self.x = torch.tensor(x, dtype=torch.float32).to(device)
+        self.z = torch.tensor(z, dtype=torch.float32).to(device)
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, index):
+        return [self.x[index], self.z[index]]
 
 
 def init_weights(m):
--- test_LIDM_spiral_2d.py
+++ test_LIDM_spiral_2d.py
@@ -4,13 +4,49 @@ import torch.optim as optim
 
 from torch.utils.data import DataLoader
 
+import LIDM
 from LIDM import *
 spiral2d_dataset = pickle.load(open("/Users/mohammad.rezaei/PycharmProjects/latent_diffusion_implicit_models/spiral2d-dataset/spiral2d_dataset.p", "rb"))
 
 x = spiral2d_dataset['observation'].reshape([spiral2d_dataset['observation'].shape[0],-1])
 z = spiral2d_dataset['state']
 device = torch.device('cpu')
-model = LIDM(latent_dim=2, obser_dim=6, device=device).to(device)
+Dataset = get_dataset(x, z, device)
+Dataset_loader = DataLoader(Dataset, batch_size=x.shape[0],shuffle=False)
+model = LIDM(latent_dim=2, obser_dim=6, sigma_x=.2,sigma_z=.1, device=device).to(device)
 model.apply(init_weights)
-# print(f'The LIDM model has {count_parameters(LIDM):,} trainable parameters')
-# optimizer = optim.Adam(model.parameters(), lr=1e-2)
+print(f'The g_theta model has {count_parameters(model.g_theta):,} trainable parameters')
+print(f'The f_phi model has {count_parameters(model.f_phi):,} trainable parameters')
+print(f'The f_phi.f_phi_x model has {count_parameters(model.f_phi.f_phi_x):,} trainable parameters')
+print(f'The LIDM model has {count_parameters(model):,} trainable parameters')
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+CLIP = 1
+total_loss=[]
+for epoch in range(200):
+    epoch_loss = 0
+    for i, batch in enumerate(Dataset_loader):
+        x, z = batch
+        x = torch.unsqueeze(x, 1)
+        z = torch.unsqueeze(z, 1)
+
+        optimizer.zero_grad()
+
+        z_hat = model(x)
+        loss=model.loss()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), CLIP)
+
+        optimizer.step()
+
+        epoch_loss += loss.item()
+    total_loss.append(epoch_loss)
+
+import matplotlib.pyplot as plt
+plt.plot(total_loss)
+plt.show()
+plt.figure()
+z_hat=z_hat.detach().cpu().numpy().squeeze()
+z=z.detach().cpu().numpy().squeeze()
+plt.plot(z_hat[:,0],z_hat[:,1], 'r')
+# plt.plot(z[:,0], z[:,1], 'k')
+plt.show()
